58b5c50f60b7873c4957199a5c4f68ce5c945b1f
gooosexe <olivershaohuang2006@gmail.com>
2025-04-15 14:56:57 -0400

    migrated to drizzle

--- src/app/api/cron/cleanup/route.ts
+++ src/app/api/cron/cleanup/route.ts
@@ -1,16 +1,7 @@
-import { PrismaClient } from "@/generated/client"; // or relative path
+import { deleteExpiredPosts } from "@/lib/actions/posts";
 import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
 export async function GET() {
-  const cutoff = new Date(Date.now() - 24 * 60 * 60 * 1000); // 24h ago
-
-  const deleted = await prisma.post.deleteMany({
-    where: {
-      createdAt: { lt: cutoff },
-    },
-  });
-
-  return NextResponse.json({ deletedCount: deleted.count });
+  await deleteExpiredPosts();
+  return NextResponse.json({ ok: true });
 }
--- src/lib/actions/posts.ts
+++ src/lib/actions/posts.ts
@@ -1,17 +1,23 @@
 "use server";
 
-import { PrismaClient } from "@/generated/client";
+import { postsTable } from "@/db/schema";
+import { drizzle } from "drizzle-orm/neon-http";
+import { lt } from "drizzle-orm";
+import { eq } from "drizzle-orm";
+import { subHours } from "date-fns";
 
-const prisma = new PrismaClient();
+const db = drizzle(process.env.DATABASE_URL!);
 
 export async function getPostById(id: number) {
   if (isNaN(id)) {
     throw new Error("Invalid post ID");
   }
 
-  const post = await prisma.post.findUnique({
-    where: { id },
-  });
+  const post = await db
+    .select()
+    .from(postsTable)
+    .where(eq(postsTable.id, id))
+    .limit(1);
 
   if (!post) {
     throw new Error("Post not found");
@@ -21,9 +27,8 @@ export async function getPostById(id: number) {
 }
 
 export async function getAllPosts() {
-  const posts = await prisma.post.findMany({
-    orderBy: { createdAt: "desc" },
-  });
+  console.log("Getting all posts");
+  const posts = await db.select().from(postsTable);
 
   return posts;
 }
@@ -37,14 +42,25 @@ export async function createPost(formData: FormData) {
     throw new Error("Missing title or content");
   }
 
-  const post = await prisma.post.create({
-    data: {
-      author: username,
-      title,
-      content,
-      createdAt: new Date(),
-    },
-  });
+  const post: typeof postsTable.$inferInsert = {
+    username: username,
+    title: title,
+    content: content,
+    createdAt: new Date(),
+  };
+
+  await db.insert(postsTable).values(post);
+  console.log("Post created");
 
   return post;
+}
+
+export async function deleteExpiredPosts() {
+  const threshold = subHours(new Date(), 24);
+
+  const deleted = await db
+    .delete(postsTable)
+    .where(lt(postsTable.createdAt, threshold));
+
+  console.log("deleted old posts:", deleted);
 }
