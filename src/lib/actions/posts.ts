9408331f581375ddf1cd284e5ee38ca252077e45
osh3276 <osh3276@gmail.xom>
2025-07-22 01:58:02 -0400

    cron job

--- src/lib/actions/posts.ts
+++ src/lib/actions/posts.ts
@@ -8,63 +8,63 @@ import { subHours } from "date-fns";
 const db = drizzle(process.env.DATABASE_URL!);
 
 export async function getPostById(id: number) {
-  if (isNaN(id)) {
-    throw new Error("Invalid post ID");
-  }
+	if (isNaN(id)) {
+		throw new Error("Invalid post ID");
+	}
 
-  const [post] = await db
-    .select()
-    .from(postsTable)
-    .where(eq(postsTable.id, id))
-    .limit(1);
+	const [post] = await db
+		.select()
+		.from(postsTable)
+		.where(eq(postsTable.id, id))
+		.limit(1);
 
-  if (!post) {
-    throw new Error("Post not found");
-  }
+	if (!post) {
+		throw new Error("Post not found");
+	}
 
-  return post;
+	return post;
 }
 
 export async function getAllPosts() {
-  console.log("Getting all posts");
-  const posts = await db
-    .select()
-    .from(postsTable)
-    .orderBy(desc(postsTable.createdAt));
+	console.log("Getting all posts");
+	const posts = await db
+		.select()
+		.from(postsTable)
+		.orderBy(desc(postsTable.createdAt));
 
-  return posts;
+	return posts;
 }
 
 export async function createPost(formData: FormData) {
-  const username = formData.get("username")?.toString() || null;
-  const title = formData.get("title")?.toString() || "";
-  const content = formData.get("content")?.toString() || "";
+	const username = formData.get("username")?.toString() || null;
+	const title = formData.get("title")?.toString() || "";
+	const content = formData.get("content")?.toString() || "";
 
-  if (!title || !content) {
-    throw new Error("Missing title or content");
-  }
+	if (!title || !content) {
+		throw new Error("Missing title or content");
+	}
 
-  console.log(username, title, content);
+	console.log(username, title, content);
 
-  const post: typeof postsTable.$inferInsert = {
-    username: username,
-    title: title,
-    content: content,
-    createdAt: new Date(),
-  };
+	const post: typeof postsTable.$inferInsert = {
+		username: username,
+		title: title,
+		content: content,
+		createdAt: new Date(),
+	};
 
-  await db.insert(postsTable).values(post);
-  console.log("Post created");
+	await db.insert(postsTable).values(post);
+	console.log("Post created");
 
-  return post;
+	return post;
 }
 
 export async function deleteExpiredPosts() {
-  const threshold = subHours(new Date(), 24);
+	const threshold = subHours(new Date(), 72);
 
-  const deleted = await db
-    .delete(postsTable)
-    .where(lt(postsTable.createdAt, threshold));
+	const deleted = await db
+		.delete(postsTable)
+		.where(lt(postsTable.createdAt, threshold));
 
-  console.log("deleted old posts:", deleted);
+	console.log("deleted old posts:", deleted);
 }
